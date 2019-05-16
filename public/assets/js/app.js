8b99b9bf6eccc2a414a84275c4d9aa1314e9fd2b
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-05-16 16:01:33 -0400

    app.js file updated

---
+++ public/assets/js/app.js
@@ -0,0 +1,60 @@
+
+
+$(function() {
+    $(".change-devoured").on('click', function(event){
+        var id = $(this).data("id");
+        var newDevoured = $(this).data('newdevoured');
+
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+    //send the PUT request
+    $.ajax('/api/burgers/' + id, {
+        type: 'PUT',
+        data: newDevouredState
+    }).then( function() {
+        console.log('changed devoured to ' + newDevoured );
+        location.reload();
+    }
+    );
+    
+    });
+
+
+        $(".create-form").on('submit', function(event) {
+            event.preventDefault();
+        
+            var newBurger = {
+                name: $('#ba').val().trim(),
+                devoured: $("[name=devoured]:checked").val().trim()
+            };
+
+            $.ajax('/api/burgers', {
+                type: 'POST',
+                data: newBurger
+            }).then(
+                function() {
+                    console.log("Burger added");
+                    location.reload();
+                }
+            );
+        });
+
+
+        $(".delete-burger").on('click', function(event) {
+            var id = $(this).data('id');
+
+            //Send the DELETE request
+
+            $.ajax('api/burgers/' + id, {
+                type: 'DELETE'
+            }).then(
+                function() {
+                    console.log('deleted burger', id);
+
+                    location.reload();
+                }
+            );
+        });
+    });
